633ba726dd7d2cf384921496123d48c846d1ece3
Sulav Neupane <neupsulav@gmail.com>
2026-06-22 19:14:06 +0545

    feat: add platform-specific slide animation to product stack navigator

--- app/(tabs)/product/_layout.tsx
+++ app/(tabs)/product/_layout.tsx
@@ -1,9 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text, Platform } from "react-native";
 import React from "react";
 import { Stack } from "expo-router";
 
 const ProductLayout = () => {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        animation: Platform.OS === "android" ? "slide_from_right" : "default",
+      }}
+    />
+  );
 };
 
 export default ProductLayout;
