cdaffebec59bcc781337091c2ae0c3bdfcc40a7a
Sulav Neupane <neupsulav@gmail.com>
2026-06-22 18:58:05 +0545

    feat: implement product details screen and navigation from product list items

---
+++ app/product/[id].tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { useLocalSearchParams } from "expo-router";
+
+const ProductDetailsScreen = () => {
+  const { id } = useLocalSearchParams();
+
+  return (
+    <View>
+      <Text>ProductDetailsScreen</Text>
+    </View>
+  );
+};
+
+export default ProductDetailsScreen;
--- components/ProductListItem.tsx
+++ components/ProductListItem.tsx
@@ -1,15 +1,21 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Pressable, TouchableOpacity } from "react-native";
 import React from "react";
 import { Product } from "@/types";
 import ImagePaths from "@/constants/ImagePaths";
+import { Link, useRouter } from "expo-router";
 
 type ProductListItemProps = {
   product: Product;
 };
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
+  const router = useRouter();
   return (
-    <View className="bg-white p-3 m-2 rounded-2xl flex-1 shadow-sm border border-gray-100 max-w-[48%]">
+    <TouchableOpacity
+      activeOpacity={0.8}
+      className="bg-white p-3 m-2 rounded-2xl flex-1 shadow-sm border border-gray-100 max-w-[48%]"
+      onPress={() => router.push(`/product/${product.id}`)}
+    >
       <Image
         source={{ uri: product.image || ImagePaths.defaultImage }}
         className="w-full aspect-square rounded-xl"
@@ -24,7 +30,7 @@ const ProductListItem = ({ product }: ProductListItemProps) => {
       <Text className="font-bold text-orange-500 mt-1">
         ${product.price.toFixed(2)}
       </Text>
-    </View>
+    </TouchableOpacity>
   );
 };
 
