67ded5571e8bb5df7029d16515b2facccc1e2670
Sulav Neupane <neupsulav@gmail.com>
2026-06-23 15:56:27 +0545

    UI: Reusable button component

---
+++ components/ButtonComponent.tsx
@@ -0,0 +1,21 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+
+const ButtonComponent = ({
+  text,
+  onPress,
+}: {
+  text: string;
+  onPress: () => void;
+}) => {
+  return (
+    <TouchableOpacity
+      className="bg-orange-500 p-4 rounded-full items-center justify-center mt-6"
+      onPress={onPress}
+    >
+      <Text className="text-white font-bold text-lg">{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default ButtonComponent;
