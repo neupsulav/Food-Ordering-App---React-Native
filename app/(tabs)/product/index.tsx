42dba091e1e91f406419f0f7fb8e03c1d2b1236a
Sulav Neupane <neupsulav@gmail.com>
2026-06-23 16:15:07 +0545

    feat: refactor menu layout to use dynamic safe area insets

--- app/(tabs)/product/index.tsx
+++ app/(tabs)/product/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FlatList, Image, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import products from "@/assets/data/products";
 import ProductListItem from "@/components/ProductListItem";
 import { Stack } from "expo-router";
@@ -8,8 +8,10 @@ import { Stack } from "expo-router";
 // const product = products[0];
 
 const MenuPage = () => {
+  const insets = useSafeAreaInsets();
+
   return (
-    <SafeAreaView className="flex-1 bg-gray-50">
+    <View className="flex-1 bg-gray-50">
       {/* for naming the header of the page */}
       <Stack.Screen options={{ title: "Menu" }} />
 
@@ -17,10 +19,13 @@ const MenuPage = () => {
         data={products}
         renderItem={({ item }) => <ProductListItem product={item} />}
         numColumns={2}
-        contentContainerClassName="p-2 pb-24"
+        contentContainerClassName="p-2"
+        contentContainerStyle={{
+          paddingBottom: insets.bottom + 16,
+        }}
         showsVerticalScrollIndicator={false}
       />
-    </SafeAreaView>
+    </View>
   );
 };
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,5 @@ import { Slot } from "expo-router";
 import "../global.css";
 
 export default function RootLayout() {
-  return (
-    // <Stack>
-    //   <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    // </Stack>
-    <Slot />
-  );
+  return <Slot />;
 }
