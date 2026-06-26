509ca2eb0965d8be539142ac91f1de96df2aa6d7
Sulav Neupane <neupsulav@gmail.com>
2026-06-26 17:22:08 +0545

    feat: integrate TanStack Query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,30 +3,33 @@ import "../global.css";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import CartProvider from "../providers/CartProvider";
 import AuthProvider from "@/providers/AuthProvider";
+import QueryProvider from "@/providers/QueryProvider";
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
       <AuthProvider>
-        <CartProvider>
-          <Stack
-            initialRouteName="index"
-            screenOptions={{ statusBarStyle: "dark" }}
-          >
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-            <Stack.Screen name="(user)" options={{ headerShown: false }} />
-            <Stack.Screen name="(admin)" options={{ headerShown: false }} />
-            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            <Stack.Screen
-              name="cart"
-              options={{
-                presentation: "modal",
-                animation: "slide_from_bottom",
-                headerShown: false,
-              }}
-            />
-          </Stack>
-        </CartProvider>
+        <QueryProvider>
+          <CartProvider>
+            <Stack
+              initialRouteName="index"
+              screenOptions={{ statusBarStyle: "dark" }}
+            >
+              <Stack.Screen name="index" options={{ headerShown: false }} />
+              <Stack.Screen name="(user)" options={{ headerShown: false }} />
+              <Stack.Screen name="(admin)" options={{ headerShown: false }} />
+              <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+              <Stack.Screen
+                name="cart"
+                options={{
+                  presentation: "modal",
+                  animation: "slide_from_bottom",
+                  headerShown: false,
+                }}
+              />
+            </Stack>
+          </CartProvider>
+        </QueryProvider>
       </AuthProvider>
     </SafeAreaProvider>
   );
