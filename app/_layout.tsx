a666f5339fb7cae5b79207b4df27c8ed7acc9e6b
Sulav Neupane <neupsulav@gmail.com>
2026-06-29 15:53:36 +0545

    feat: implement push notification support using expo-notifications and integrate NotificationProvider into root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,31 +4,34 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import CartProvider from "../providers/CartProvider";
 import AuthProvider from "@/providers/AuthProvider";
 import QueryProvider from "@/providers/QueryProvider";
+import NotificationProvider from "@/providers/NotificationProvider";
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
       <AuthProvider>
         <QueryProvider>
-          <CartProvider>
-            <Stack
-              initialRouteName="index"
-              screenOptions={{ statusBarStyle: "dark" }}
-            >
-              <Stack.Screen name="index" options={{ headerShown: false }} />
-              <Stack.Screen name="(user)" options={{ headerShown: false }} />
-              <Stack.Screen name="(admin)" options={{ headerShown: false }} />
-              <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-              <Stack.Screen
-                name="cart"
-                options={{
-                  presentation: "modal",
-                  animation: "slide_from_bottom",
-                  headerShown: false,
-                }}
-              />
-            </Stack>
-          </CartProvider>
+          <NotificationProvider>
+            <CartProvider>
+              <Stack
+                initialRouteName="index"
+                screenOptions={{ statusBarStyle: "dark" }}
+              >
+                <Stack.Screen name="index" options={{ headerShown: false }} />
+                <Stack.Screen name="(user)" options={{ headerShown: false }} />
+                <Stack.Screen name="(admin)" options={{ headerShown: false }} />
+                <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+                <Stack.Screen
+                  name="cart"
+                  options={{
+                    presentation: "modal",
+                    animation: "slide_from_bottom",
+                    headerShown: false,
+                  }}
+                />
+              </Stack>
+            </CartProvider>
+          </NotificationProvider>
         </QueryProvider>
       </AuthProvider>
     </SafeAreaProvider>
---
+++ providers/NotificationProvider.tsx
@@ -0,0 +1,48 @@
+import { registerForPushNotificationsAsync } from "@/lib/notifications";
+import { PropsWithChildren, useEffect, useState } from "react";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
+
+const NotificationProvider = ({ children }: PropsWithChildren) => {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState<
+    Notifications.Notification | undefined
+  >(undefined);
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+      .then((token) => setExpoPushToken(token ?? ""))
+      .catch((error: any) => setExpoPushToken(`${error}`));
+
+    const notificationListener = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        setNotification(notification);
+      },
+    );
+
+    const responseListener =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      notificationListener.remove();
+      responseListener.remove();
+    };
+  }, []);
+
+  console.log("push token:", expoPushToken);
+  console.log("notification:", notification);
+
+  return <>{children}</>;
+};
+
+export default NotificationProvider;
